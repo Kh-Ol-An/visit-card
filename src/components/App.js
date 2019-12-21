3c8a5a0f20605d4225da2a24ad0fbfb46f527e1c
Kh-Ol-An <olegkhristenko@gmail.com>
2019-12-21 17:26:47 +0200

    lint husky pret rem

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Main from './components/Main/Main';
-import Contacts from './components/Contacts/Contacts';
+import Main from './Main/Main';
+import Contacts from './Contacts/Contacts';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './App';
+import App from './components/App';
 import './styles/normalize.css';
 import './styles/reset.css';
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.render(
   <BrowserRouter>
